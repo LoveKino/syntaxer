88566196362ea8ed2c867dc4f35d20ac1fdb97a0
loveKino <842914439@qq.com>
2017-03-22 00:50:55 +0800

    doc

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+'use strict';
+
+/**
+ * @readme-quick-run
+ *
+ * build LR1 table
+ *
+ * ## test tar=js r_c=syntaxer
+ *
+ * let {buildLR1Table} = syntaxer;
+ * let lr1Table = buildLR1Table({
+ *     startSymbol: 'S',
+ *     N: ['S'],
+ *     T: ['a'],
+ *     productions: [
+ *         ['S', ['a']] // s -> a
+ *     ]
+ * });
+ * console.log(JSON.stringify(lr1Table, null, 4));
+ */
+
+/**
+ * @readme-quick-run
+ *
+ * generate ast from LR table
+ *
+ * ## test tar=js r_c=syntaxer
+ *
+ * let {buildLR1Table, LR} = syntaxer;
+ * let {ACTION, GOTO} = buildLR1Table({
+ *     startSymbol: 'S',
+ *     N: ['S'],
+ *     T: ['a'],
+ *     productions: [
+ *         ['S', ['a']] // s -> a
+ *     ]
+ * });
+ * let lrParse = LR(ACTION, GOTO);
+ * lrParse({ // accept a taken
+ *   name: 'a',
+ *   text: 'abc'
+ * });
+ * let ast = lrParse(null); // null as end symbol
+ * console.log(ast);
+ */
 module.exports = require('./src');
